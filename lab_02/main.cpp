9fbdf7b98aae6e96e08eda2de44d9a7ecb3b493e
klishin16 <klishin.nd16@gmail.com>
2020-07-09 14:56:34 +0300

    Исправлены ошибки 2 лр

--- lab_02/main.cpp
+++ lab_02/main.cpp
@@ -8,35 +8,53 @@ public:
         return value;
     };
 
-public: void set_value(long long number) {
+    void set_value(long long number) {
         this->value = number;
     };
 
-public: safe_long_long_t() {
+    safe_long_long_t() {
         value = 0;
     };
 
-public: safe_long_long_t(long long number) {
+    safe_long_long_t(long long number) {
         value = number;
     };
 
-public: void operator= (long long number) {
+    void operator= (long long number) {
         this->value = number;
     };
 
-public: safe_long_long_t operator+ (safe_long_long_t number) {
+    safe_long_long_t operator+ (safe_long_long_t number) {
         return safe_long_long_t(value + number.get_value());
     };
 
-public: safe_long_long_t operator- (safe_long_long_t number) {
+    safe_long_long_t operator- (safe_long_long_t number) {
         return safe_long_long_t(value - number.get_value());
     };
 
-public: safe_long_long_t operator* (safe_long_long_t number) {
+    safe_long_long_t operator* (safe_long_long_t number) {
         return safe_long_long_t(value * number.get_value());
     };
 
-public:
+    safe_long_long_t operator++() {
+        value++;
+        return *this;
+    }
+    safe_long_long_t operator+=(safe_long_long_t inc) {
+        value += inc.get_value();
+        return *this;
+    }
+
+    safe_long_long_t operator--() {
+        value--;
+        return *this;
+    }
+
+    safe_long_long_t operator-=(safe_long_long_t dec) {
+        value-=dec.get_value();
+        return *this;
+    }
+
     bool operator== (safe_long_long_t number) {
         return (this->value == number.get_value());
     };
@@ -57,11 +75,13 @@ public:
     };
 
 
-public: friend std::ostream &operator<< (std::ostream &out, safe_long_long_t number);
-public: friend std::istream &operator>> (std::istream &in, safe_long_long_t &number);
-public: friend safe_long_long_t operator+(int l, safe_long_long_t& r);
-public: friend safe_long_long_t operator-(int l, safe_long_long_t& r);
-public: friend safe_long_long_t operator*(int l, safe_long_long_t& r);
+public:
+    friend std::ostream &operator<< (std::ostream &out, safe_long_long_t number);
+    friend std::istream &operator>> (std::istream &in, safe_long_long_t &number);
+    friend safe_long_long_t operator+(int l, safe_long_long_t& r);
+    friend safe_long_long_t operator-(int l, safe_long_long_t& r);
+    friend safe_long_long_t operator*(int l, safe_long_long_t& r);
+    friend safe_long_long_t operator+=(int inc, safe_long_long_t& l);
 };
 
 std::ostream& operator<< (std::ostream &out, safe_long_long_t number) {
@@ -83,7 +103,11 @@ static safe_long_long_t operator- (int l, safe_long_long_t &r) {
 };
 
 static safe_long_long_t operator* (int l, safe_long_long_t &number) {
-    return safe_long_long_t(l*number.get_value());
+    return {l*number.get_value()};
+};
+
+safe_long_long_t operator+=(int inc, safe_long_long_t &l) {
+    return {l.value + inc};
 };
 
 int main() {
@@ -107,5 +131,10 @@ int main() {
     std::cout << "Int(5) + Number1: " << (int(5) + number1) << std::endl;
     std::cout << "Int(5) - Number1: " << (int(5) - number1) << std::endl;
     std::cout << "Int(5) * Number1: " << (int(5) * number1) << std::endl;
+    std::cout << "Number1 += Number2: " << (number1+= number2) << std::endl;
+    std::cout << "++Number1: " << ++number1 << std::endl;
+    std::cout << "--Number1: " << --number1 << std::endl;
     return 0;
 }
+
+
--- lab_02/main2.cpp
+++ lab_02/main2.cpp
@@ -2,58 +2,74 @@
 
 class safe_c_array_t {
 public:
-    int* adrr;
+    int* adrr = nullptr;
     int length = 0;
 
     long long get_length() {
         return length;
     };
 
-    safe_c_array_t(int len) {
+    explicit safe_c_array_t(int len) {
         length = len;
         adrr = new int[length];
+        for (int i = 0; i < length; i++) {
+            adrr[i] = 0;
+        }
     };
 
     void remove() {
         delete [] adrr;
     }
 
-    int& operator[] (const int index);
-
     safe_c_array_t(safe_c_array_t& other_arr) {
-        delete [] adrr;
+        if (adrr != nullptr) {
+            delete [] adrr;
+        }
         adrr = new int[other_arr.get_length()];
         length = other_arr.get_length();
         for (int i = 0; i < other_arr.get_length(); i++) {
             adrr[i] = other_arr[i];
         };
     };
-
+    int& operator[] (int index) const;
+    safe_c_array_t& operator=(safe_c_array_t &other);
+    friend std::ostream &operator<< (std::ostream &out, safe_c_array_t array);
 };
 
-int& safe_c_array_t::operator[] (const int index) {
+int& safe_c_array_t::operator[] (const int index) const {
     return adrr[index];
 }
 
-void show_array(safe_c_array_t& arr) {
-    for (int i = 0; i < arr.get_length(); i++) {
-        std::cout << arr[i] << ' ';
-    };
+safe_c_array_t &safe_c_array_t::operator=(safe_c_array_t &other) {
+    delete [] adrr;
+    adrr = new int[other.length];
+    for(int i = 0; i < other.length; i++) {
+        adrr[i] = other[i];
+    }
+    return *this;
 }
 
-
+std::ostream &operator<<(std::ostream &out, safe_c_array_t array) {
+    for (int i = 0; i < array.length; i++) {
+        out << array[i] << ' ';
+    }
+    out << std::endl;
+    return out;
+}
 
 int main() {
     safe_c_array_t arr_0(3);
-    std::cout << arr_0.get_length() << std::endl;
-    show_array(arr_0);
-    std::cout << std::endl;
+    std::cout << "Array arr_0 length: " << arr_0.get_length() << std::endl;
+    std::cout << arr_0;
     arr_0[2] = 1;
-    show_array(arr_0);
-    std::cout << std::endl;
+    std::cout << arr_0;
 
     safe_c_array_t arr_1 = arr_0;
-    show_array(arr_1);
+    std::cout << arr_1;
+
+    safe_c_array_t arr_2(3);
+    arr_2 = arr_1;
+    std::cout << arr_2;
 
     return 0;
 }
